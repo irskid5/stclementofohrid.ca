9846d7441d85b23c214bced09280b4c218dbf352
Vele.Tosevski <vele.tosevski@moneris.com>
2019-03-25 15:20:28 -0400

    Updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `St. Clement of Ohrid`,
+    description: `Official website of St.Clement of Ohrid Macedonian Orthodox Cathedral Toronto, Ontario.  Visit us for more info on events and our banquet halls`,
+    author: `@VeleTosevski`,
+    keywords:
+      "Macedonia, Orthodox, Banquet Hall, Events, Parties, Christianity",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,10 +26,10 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-netlify-cms`
+    `gatsby-plugin-netlify-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
--- src/components/seo.js
+++ src/components/seo.js
@@ -10,6 +10,8 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+import favicon from "../images/favicon.png"
+
 function SEO({ description, lang, meta, keywords, title }) {
   const { site } = useStaticQuery(
     graphql`
@@ -77,6 +79,14 @@ function SEO({ description, lang, meta, keywords, title }) {
             : []
         )
         .concat(meta)}
+      link={[
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: `${favicon}`,
+        },
+      ]}
     />
   )
 }
