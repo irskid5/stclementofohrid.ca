d72be734580a906796693fd0bf810249f2a97b71
Vele.Tosevski <vele.tosevski@moneris.com>
2019-03-30 16:46:12 -0400

    made layout top component so it does not change

--- src/pages/banquethalls.js
+++ src/pages/banquethalls.js
@@ -4,11 +4,11 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const BanquetHallsPage = () => (
-  <Layout>
+  <div>
     <SEO title="...In Construction" />
     <h1>In Construction</h1>
     <p>You just hit a route that is still in construction... the sadness.</p>
-  </Layout>
+  </div>
 )
 
 export default BanquetHallsPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <div>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-  </Layout>
+  </div>
 )
 
 export default IndexPage
