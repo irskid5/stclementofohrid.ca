1f214ed0ecd6ef0496fadaa6f000715b0fb3994e
Vele.Tosevski <vele.tosevski@moneris.com>
2019-06-10 16:46:19 -0400

    added underline to nav links

--- src/components/header.js
+++ src/components/header.js
@@ -22,18 +22,26 @@ const Header = ({ siteTitle, menuLinks, headerImage }) => (
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Link className="nav-link" to="/about">
-          About
-        </Link>
-        <Link className="nav-link" to="/spiritual">
-          Spiritual
-        </Link>
-        <Link className="nav-link" to="/banquethalls">
-          Banquet Halls
-        </Link>
-        <Link className="nav-link" to="/events">
-          Events
-        </Link>
+        <Nav.Link>
+          <Link className="nav-link-new" to="/about">
+            About
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link className="nav-link-new" to="/spiritual">
+            Spiritual
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link className="nav-link-new" to="/banquethalls">
+            Banquet Halls
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link className="nav-link-new" to="/events">
+            Events
+          </Link>
+        </Nav.Link>
         <NavDropdown title="Groups" id="basic-nav-dropdown">
           <Link className="dropdown-item" to="/groups">
             Ensemble Makedonka
