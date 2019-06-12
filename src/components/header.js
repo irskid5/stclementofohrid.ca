2fba9ab6748d0cd794c17e90720bedf2b912be5f
Vele.Tosevski <vele.tosevski@moneris.com>
2019-06-12 18:04:41 -0400

    changed header to react component, added color change on scroll

--- src/components/header.js
+++ src/components/header.js
@@ -1,81 +1,102 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { Component } from "react"
 // import Img from "gatsby-image"
 import Link from "gatsby-link"
 
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  // Form,
-  // FormControl,
-  // Button,
-} from "react-bootstrap"
+import { Navbar, Nav, NavDropdown } from "react-bootstrap"
 
 import "./header.css"
 
-const Header = ({ siteTitle, menuLinks, headerImage }) => (
-  <Navbar bg="light" expand="lg" sticky="top" id="header">
-    <div className="box-links">
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Link className="nav-link" to="/about">
-            About
-          </Link>
-          <Link className="nav-link" to="/spiritual">
-            Spiritual
-          </Link>
-          <Link className="nav-link" to="/banquethalls">
-            Banquet Halls
-          </Link>
-          <Link className="nav-link" to="/events">
-            Events
-          </Link>
-          <NavDropdown title="Groups" id="basic-nav-dropdown">
-            <Link className="dropdown-item" to="/groups">
-              Ensemble Makedonka
-            </Link>
-            <Link className="dropdown-item" to="/groups">
-              Ladies Auxilary
-            </Link>
-            <Link className="dropdown-item" to="/groups">
-              Bowling
-            </Link>
-            <Link className="dropdown-item" to="/groups">
-              Sunday School
-            </Link>
-            <Link className="dropdown-item" to="/groups">
-              MYNET
-            </Link>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </div>
-    <div className="box-logo">
-      <Link className="nav-brand" to="/">
-        <img src={headerImage} alt="Logo" className="navbarLogo" />
-      </Link>
-    </div>
-    <div className="box-hamburger">
-      <Navbar.Toggle
-        aria-controls="responsive-navbar-nav"
-        id="toggler-hamburger"
+class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isTop: true,
+    }
+
+    this.onScroll = this.onScroll.bind(this)
+  }
+
+  componentDidMount() {
+    document.addEventListener("scroll", () => {
+      const isTop = window.scrollY < 50
+      if (isTop !== this.state.isTop) {
+        this.onScroll(isTop)
+      }
+    })
+  }
+
+  onScroll(isTop) {
+    this.setState({ isTop: isTop })
+  }
+
+  render() {
+    return (
+      <Navbar
+        expand="lg"
+        sticky="top"
+        id="header"
+        className={this.state.isTop ? "" : "scroll"}
       >
-        <span className="icon-bar" />
-        <span className="icon-bar" />
-        <span className="icon-bar" />
-        <span className="sr-only">Toggle navigation</span>
-      </Navbar.Toggle>
-    </div>
-  </Navbar>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+        <div className="box-links">
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+              <Link className="nav-link" to="/spiritual">
+                Spiritual
+              </Link>
+              <Link className="nav-link" to="/banquethalls">
+                Banquet Halls
+              </Link>
+              <Link className="nav-link" to="/events">
+                Events
+              </Link>
+              <NavDropdown title="Groups" id="basic-nav-dropdown">
+                <Link className="dropdown-item" to="/groups">
+                  Ensemble Makedonka
+                </Link>
+                <Link className="dropdown-item" to="/groups">
+                  Ladies Auxilary
+                </Link>
+                <Link className="dropdown-item" to="/groups">
+                  Bowling
+                </Link>
+                <Link className="dropdown-item" to="/groups">
+                  Sunday School
+                </Link>
+                <Link className="dropdown-item" to="/groups">
+                  MYNET
+                </Link>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+        <div className="box-logo">
+          <Link className="nav-brand" to="/">
+            <img
+              src={this.props.headerImage}
+              alt="Logo"
+              className="navbarLogo"
+            />
+          </Link>
+        </div>
+        <div className="box-hamburger">
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav"
+            id="toggler-hamburger"
+          >
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="sr-only">Toggle navigation</span>
+          </Navbar.Toggle>
+        </div>
+      </Navbar>
+    )
+  }
 }
 
 export default Header
