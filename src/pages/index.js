74eea08f5c7e157ee2aa660edda642f4eb41c6f4
Vele Tosevski <vele97@gmail.com>
2019-09-11 02:21:14 -0400

    tidied up index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,35 +6,64 @@ import SEO from "../components/seo"
 
 import "./index.css"
 
-const IndexPage = () => (
-  <StaticQuery
-    query={query}
-    render={data => (
-      <div id="body" className="mainBody">
-        <SEO
-          title="Home"
-          keywords={[
-            `St.Clement`,
-            `Church`,
-            `Macedonia`,
-            `Orthodox`,
-            `Cathedral`,
-            `Christianity`,
-          ]}
-        />
-        <Img
-          fluid={data.fileName.childImageSharp.fluid}
-          alt="Picture of St.Clement of Ohrid MOC"
-          className="startingImage"
-        />
-      </div>
-    )}
-  />
-)
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <StaticQuery
+        query={query}
+        render={data => (
+          <div id="body" className="mainBody">
+            <SEO
+              title="Home"
+              keywords={[
+                `St.Clement`,
+                `Church`,
+                `Macedonia`,
+                `Orthodox`,
+                `Cathedral`,
+                `Christianity`,
+              ]}
+            />
+            <div className="mainImageContainer">
+              <Img
+                fluid={data.fileName1.childImageSharp.fluid}
+                alt="Picture of St.Clement of Ohrid MOC"
+                className="startingImage"
+              />
+              <div className="welcomeTextContainer">
+                <h1>Welcome to St. Clement of Ohrid</h1>
+                <h2>Macedonian Orthodox Cathedral</h2>
+                <h2>Toronto, Canada</h2>
+              </div>
+            </div>
+            <div className="sermonImageContainer">
+              <Img
+                fluid={data.fileName2.childImageSharp.fluid}
+                alt="Picture of St.Clement of Ohrid MOC"
+                className="startingImage"
+              />
+            </div>
+          </div>
+        )}
+      />
+    )
+  }
+}
 
 const query = graphql`
   query {
-    fileName: file(relativePath: { eq: "images/test_index_photo_2.jpg" }) {
+    fileName1: file(relativePath: { eq: "images/indexImage2.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 10000, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    fileName2: file(relativePath: { eq: "images/imageOfChurchInside.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 10000, quality: 100) {
           ...GatsbyImageSharpFluid
