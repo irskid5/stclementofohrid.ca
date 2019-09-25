6ad72b049333a75adb1c463f6b1d70fe93a7e1ce
Vele Tosevski <vele97@gmail.com>
2019-09-25 01:14:40 -0400

    added text and button for sermon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-
+import Link from "gatsby-link"
 import "animate.css/animate.min.css"
-
 import ScrollAnimation from "react-animate-on-scroll"
+import { Button } from "react-bootstrap"
 
 import SEO from "../components/seo"
 
@@ -57,32 +57,34 @@ class IndexPage extends React.Component {
                 `Christianity`,
               ]}
             />
-            <div className="mainImageContainer">
-              <div className="welcomeTextContainer">
-                <h1>Welcome to St. Clement of Ohrid</h1>
-                <h2>Macedonian Orthodox Cathedral</h2>
-                <h2>Toronto, Canada</h2>
+            <ScrollAnimation animateIn="fadeIn" duration={0.5}>
+              <div className="mainImageContainer">
+                <div className="welcomeTextContainer">
+                  <h1>Welcome to St. Clement of Ohrid</h1>
+                  <h2>Macedonian Orthodox Cathedral</h2>
+                  <h2>Toronto, Canada</h2>
+                </div>
+                {this.state.size === "m" ? (
+                  <Img
+                    fluid={data.fileName2.childImageSharp.fluid}
+                    alt="Picture of St.Clement of Ohrid"
+                    className="startingImage"
+                  />
+                ) : this.state.size === "t" ? (
+                  <Img
+                    fluid={data.fileName2.childImageSharp.fluid}
+                    alt="Picture of St.Clement of Ohrid"
+                    className="startingImage"
+                  />
+                ) : (
+                  <Img
+                    fluid={data.fileName1.childImageSharp.fluid}
+                    alt="Picture of St.Clement of Ohrid"
+                    className="startingImage"
+                  />
+                )}
               </div>
-              {this.state.size === "m" ? (
-                <Img
-                  fluid={data.fileName2.childImageSharp.fluid}
-                  alt="Picture of St.Clement of Ohrid"
-                  className="startingImage"
-                />
-              ) : this.state.size === "t" ? (
-                <Img
-                  fluid={data.fileName2.childImageSharp.fluid}
-                  alt="Picture of St.Clement of Ohrid"
-                  className="startingImage"
-                />
-              ) : (
-                <Img
-                  fluid={data.fileName1.childImageSharp.fluid}
-                  alt="Picture of St.Clement of Ohrid"
-                  className="startingImage"
-                />
-              )}
-            </div>
+            </ScrollAnimation>
             <ScrollAnimation
               animateIn="fadeIn"
               animateOut="fadeOut"
@@ -95,6 +97,22 @@ class IndexPage extends React.Component {
                   className="startingSermonImage"
                 />
                 <div className="shadownOnImage" />
+                <div className="lastWeekSermonContainer">
+                  <div className="lastWeekSermonText">
+                    <a>Want To Know More About Last Week's Sermon?</a>
+                  </div>
+                  <div className="lastWeekSermonButton">
+                    <Link to="/">
+                      <Button
+                        variant="outline-secondary"
+                        style={{ color: "#ffffff", borderColor: "#ffffff" }}
+                        size="lg"
+                      >
+                        Read More
+                      </Button>
+                    </Link>
+                  </div>
+                </div>
               </div>
             </ScrollAnimation>
           </div>
