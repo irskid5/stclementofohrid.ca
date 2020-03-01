d61ff44663a4d14929f78c628a7537c7f323212a
Vele Tosevski <vele97@gmail.com>
2020-02-29 19:10:53 -0500

    added something to MOCHistory.js so netlify can shut up

---
+++ src/pages/MOCHistory.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import SEO from "../components/seo"
+
+const MOCHistoryPage = () => (
+  <>
+    <SEO title="...In Construction" />
+    <h1>In Construction</h1>
+    <p>You just hit a route that is still in construction... the sadness.</p>
+  </>
+)
+
+export default SpiritualPage
